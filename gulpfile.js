74a3b32ceedca62ed187350ee71fbe62ca1e4644
tomadj <thomas.adjadj@outlook.com>
2015-07-14 21:24:04 +0200

    serve

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ require('./tasks/build');
 require('./tasks/sass');
 require('./tasks/watch');
 require('./tasks/test');
-require('./tasks/server');
+require('./tasks/serve');
 
 gulp.task('build', ['browserify', 'sass', 'fonts', 'assets','templates']);
 gulp.task('release', ['browserify:release', 'sass', 'fonts', 'assets','templates']);
